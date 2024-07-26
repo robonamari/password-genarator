b9fa275ca568022e1d237c5239d7c424fb21b9ec
robonamari <ROBONAMARI@GMAIL.COM>
2024-07-26 11:59:33 +0330

    Improvement of password generator

--- main.py
+++ main.py
@@ -1,1 +1,1 @@
-print("Your Password:\n"+ "".join([__import__("random").choice("abcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()?") for i in range(16)]))
+print("Your Password:\n"+ "".join([__import__("random").choice("AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789!@#$%^&*()?") for i in range(16)]))
